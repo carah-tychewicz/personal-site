2637d0f73cb0dce707ef3f7aa5af4df86c8100e4
tiffany-tychewicz <tychewicz.design.studio@gmail.com>
2021-11-29 15:36:06 -0600

    orbit controls work!

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,16 +1,36 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useEffect } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from "three";
+
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  useEffect(
+    () => {
+      const controls = new OrbitControls(camera, gl.domElement);
+
+      controls.minDistance = 3;
+      controls.maxDistance = 20;
+      return () => {
+        controls.dispose();
+      };
+    },
+    [camera, gl]
+  );
+  return null;
+};
 
 const Box = () => {
   return (
     <div id="canvas-container">
-      <Canvas>
+      <Canvas>  
+        <CameraController />
         <ambientLight intensity={0.1} />
         <directionalLight color="royalblue" position={[0, 0, 5]} />
-        <mesh>
-          <octahedronGeometry />
-          <meshStandardMaterial />
-        </mesh>
+          <mesh>
+            <cylinderGeometry />
+            <meshStandardMaterial />
+          </mesh>
       </Canvas>
     </div>
   );
